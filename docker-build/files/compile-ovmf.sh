c1e5a993a33067c43bad0f82f814c58d6603808f
breadhandsome15048 <93503527+breadhandsome15048@users.noreply.github.com>
2026-03-17 18:40:22 +0800

    Modify git commands in compile-ovmf.sh
    Updated git commands to clean and initialize submodules.

--- docker-build/files/compile-ovmf.sh
+++ docker-build/files/compile-ovmf.sh
@@ -19,11 +19,12 @@ export EDK_TOOLS_PATH="${SRC_DIR}/BaseTools"
 cd ${SRC_DIR}
 mkdir -p bin
 ln -sf /usr/bin/python3 bin/python
+git clean -xfd
 git pull
 #git checkout vUDK2018
 git checkout ba0e0e4
 git pull --recurse-submodules
-git submodule update --recursive
+git submodule update --init --recursive
 
 # Build Basetools
 make -C BaseTools
